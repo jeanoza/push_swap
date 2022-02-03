eefcb833fd054a723f56423e6342e12b37483f3e
jeanoza <choikb916@gmail.com>
2022-02-03 10:57:15 +0100

    feat:commit for connect with remote repo

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: kyubongchoi <kyubongchoi@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/01 21:51:46 by kyubongchoi       #+#    #+#             */
-/*   Updated: 2022/02/01 22:20:40 by kyubongchoi      ###   ########.fr       */
+/*   Updated: 2022/02/02 22:06:29 by kyubongchoi      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,19 @@
 #include <stdio.h>
 #include <string.h>
 
-void	init_stack(int ac, char **av, int **a, int **b)
+int	*init_stack(int ac, char **av)
 {
 	int	i;
+	int	*stack;
 
-	*a = malloc(sizeof(int) * ac);
-	*b = malloc(sizeof(int) * ac);
+	stack = malloc(sizeof(int) * ac);
 	i = 0;
 	while (i < ac)
 	{
-		(*a)[i] = atoi(av[i]);
+		stack[i] = atoi(av[i]);
 		++i;
 	}
+	return (stack);
 }
 
 int	main(int ac, char **av)
@@ -36,13 +37,35 @@ int	main(int ac, char **av)
 	int	*stack_a;
 	int	*stack_b;
 
-	init_stack(ac - 1, av + 1, &stack_a, &stack_b);
+	stack_a = init_stack(ac - 1, av + 1);
 
 	int i = -1;
 	while (++i < ac - 1)
 		printf("(%d)%d\n", i, stack_a[i]);
 		
 	free(stack_a);
-	free(stack_b);
+	while (1);
 	return (0);
 }
+
+/*
+sa : swap a - swap the first 2 elements at the top of stack a.
+	Do nothing if there is only one or no elements).
+sb : swap b - swap the first 2 elements at the top of stack b.
+	Do nothing if there is only one or no elements).
+ss : sa and sb at the same time.
+pa : push a - take the first element at the top of b and put it at the top of a.
+	Do nothing if b is empty.
+pb : push b - take the first element at the top of a and put it at the top of b.
+	Do nothing if a is empty.
+ra : rotate a - shift up all elements of stack a by 1.
+	The first element becomes the last one.
+rb : rotate b - shift up all elements of stack b by 1.
+	The first element becomes the last one.
+rr : ra and rb at the same time.
+rra : reverse rotate a - shift down all elements of stack a by 1.
+	The last element becomes the first one.
+rrb : reverse rotate b - shift down all elements of stack b by 1.
+	The last element becomes the first one.
+rrr : rra and rrb at the same time.
+*/
