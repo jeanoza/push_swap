cf976565c85725b4f187b1e9783d295bf1ccb42c
kychoi <kychoi@e1r3p20.42.fr>
2022-02-11 11:22:46 +0100

    hmm

--- main.js
+++ main.js
@@ -1,3 +1,1 @@
 const QuickSort = require('./quicksort');
-
-console.log(QuickSort([1,2,3,4,5], 0, 1));
--- quicksort.js
+++ quicksort.js
@@ -1,5 +1,6 @@
 /** @param {Number[]} arr */
-module.exports = function QuickSort (arr, left, right) {
+// module.exports = function QuickSort (arr, left, right) {
+function QuickSort (arr, left, right) {
     let pivot;
     let index;
     if (left < right) {
@@ -11,19 +12,25 @@ module.exports = function QuickSort (arr, left, right) {
     return arr;
 }
 function Partition (arr, pivot, left, right){
-    let value = arr[pivot];
 	let index = left;
     for (let i = left; i < right; i++) {
-        if (arr[i] < value) {
+        // console.log("in:", arr, "i:", i, "index", index, "right:", right);
+        if (arr[i] < arr[pivot]) {
+            console.log("in:", arr, "i:", i, "index", index, "right:", right, "pivot:", pivot);
             Swap(arr, i, index);
             index++;
         }
     }
     Swap(arr, right, index);
+    // console.log("partition:", arr);
     return index;
 }
 function Swap (arr, i, j) {
     let tmp = arr[i];
     arr[i] = arr[j];
     arr[j] = tmp;
+    console.log("swap:", arr, i, j);
 }
+
+// console.log(QuickSort([2,1,3,4,8,6,5], 0, 6));
+console.log(QuickSort([5,4,6,2,1,3], 0, 5));
